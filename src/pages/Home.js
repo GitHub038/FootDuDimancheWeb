2ba67c952d676ead4f3b4ca77a46d53aac4eca20
sao <c_pasmoi@outlook.com>
2022-07-05 20:20:07 +0200

    apply website style green and write

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
   const { currentUser } = useContext(UserContext);
   return (
     <div className="container p-5">
-      <h1 className="display-3 text-light">
+      <h1 className="display-3">
         {currentUser
           ? "Bienvenue"
           : "Salut, connecte toi pour accéder aux matchs"}
