7ae53fd1dc61ba9736d82cdd9cf2191093b41f7b
Sao dev <salim.otmani@outlook.com>
2022-07-05 08:49:06 +0200

    fix autorun git (#3)
    remove warning and update npm build command
    
    Co-authored-by: sao <c_pasmoi@outlook.com>

--- src/pages/Private/PrivateHome/PrivateHome.js
+++ src/pages/Private/PrivateHome/PrivateHome.js
@@ -4,7 +4,7 @@ export default function PrivateHome() {
   return (
     <div className="container p-5">
       <h1 className="display-3 text-light mb-4">Home Sweet Private Home</h1>
-      <img src={cat} />
+      <img src={cat} alt="" />
     </div>
   );
 }
